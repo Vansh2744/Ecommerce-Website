fceef35997c794d3724fba917f0186181b41bc18
vansh <vanshchoudhary2744@gmail.com>
2025-01-03 16:19:46 +0530

    edit cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json({ limit: "10kb" }));
 app.use(cookieParser());
 app.use(express.static("public"));
-app.use(cors());
+app.use(cors(
+    {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+    }
+));
 
 // Routes
 
