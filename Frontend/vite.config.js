5f87fec1d1dc8f43ad26dc316e8addf6b228a806
vansh <vanshchoudhary2744@gmail.com>
2025-01-03 17:23:17 +0530

    created api link of backend

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-        "/api": "http://localhost:3000",
+        "/api": "https://ecommerce-website-three-umber-53.vercel.app/",
     },
   },
 });
