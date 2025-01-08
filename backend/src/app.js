bf8a69acd3aca7c8f05651b20f37f93bf63e44b4
Vansh <vanshchoudhary2744@gmail.com>
2025-01-08 19:47:27 +0530

    updated ports for deployment

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,7 @@ app.use(cookieParser());
 app.use(express.static("public"));
 app.use(cors(
     {
-        origin: "http://localhost:5173",
+        origin: "https://frontendecom-two.vercel.app",
         methods: ["GET", "POST", "PUT", "DELETE"],
         allowedHeaders: ["Content-Type", "Authorization"],
     }
