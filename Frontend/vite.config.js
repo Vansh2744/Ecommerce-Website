8db39dbabbfaf773181ee4478dbea817577bcca2
Vansh <vanshchoudhary2744@gmail.com>
2025-01-15 11:12:26 +0530

    changed proxy server

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,12 +3,10 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   server: {
     proxy: {
-      "/api": {
-        target: "https://backendecom-two.vercel.app",
-      },
+      "/api": "https://backendecom-two.vercel.app",
     },
   },
+  plugins: [react()],
 });
