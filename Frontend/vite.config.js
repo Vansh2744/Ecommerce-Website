766e5d8f6ca0df2964b46ac195200ef1e0795063
Vansh <118882606+Vansh2744@users.noreply.github.com>
2025-01-15 11:22:35 +0530

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,7 +5,11 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "https://backendecom-two.vercel.app",
+      '/api': {
+        target: 'https://backendecom-two.vercel.app',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
     },
   },
   plugins: [react()],
